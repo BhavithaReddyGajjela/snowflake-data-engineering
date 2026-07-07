97f0385feec23569ade762e89422669bf5b732d5
BhavithaReddyGajjela <bredg27@gmail.com>
2026-07-07 01:21:50 -0400

    Update streamlit_app.py
    Display API response in dataframe. Formatted the JSON response into a Streamlit dataframe for easier viewing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,6 +53,11 @@ if time_to_insert:
     icon="✅"
 ) 
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+import requests
+smoothiefroot_response = requests.get(
+    "https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+st.dataframe(
+    data=smoothiefroot_response.json(),
+    use_container_width=True
+)
